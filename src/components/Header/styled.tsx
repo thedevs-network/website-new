e45b2909d21e1a887931f69d36cb71de75d708db
Taha <taha.ojari@gmail.com>
2021-07-02 13:33:15 +0430

    fix: better spacing in menu-item

--- src/components/Header/styled.tsx
+++ src/components/Header/styled.tsx
@@ -3,18 +3,20 @@ import styled from "@emotion/styled";
 export const Menu = styled.ul`
   list-style: none;
   display: flex;
+  margin: 0;
 `;
 
 export const MenuItem = styled.li`
   display: flex;
   align-items: center;
   color: white;
+  padding: 5px;
 
   &:not(:last-child) {
-    margin-right: 20px;
+    margin-right: 10px;
   }
 
-  /* dot before item */
+  /* dot before each item */
   &:not(:first-child) {
     &:before {
       display: block;
