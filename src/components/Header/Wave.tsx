d0752994a6854ddfd87ca9c581dfb1d26c87e1f2
Taha <taha.ojari@gmail.com>
2021-07-02 13:52:26 +0430

    fix(header): wave position

--- src/components/Header/Wave.tsx
+++ src/components/Header/Wave.tsx
@@ -11,6 +11,7 @@ const Wave = () => {
       width="100%"
       height="100%"
       viewBox="0 0 1440 400"
+      style={{ transform: "translateY(-120px)" }}
     >
       <defs>
         <linearGradient id="gradient">
